4f6431e01f5e92006b6a50ea475c838a08d2560e
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-08-18 08:19:26 +0200

    V.2 - add data validation

--- _test/Office/xls-resetExcelSheet.vb
+++ _test/Office/xls-resetExcelSheet.vb
@@ -1,3 +1,6 @@
+'
+'  V.2
+'
 option explicit
 
 sub main() ' {
@@ -21,12 +24,49 @@ sub init_sheet() ' {
     activeWindow.panes(2).scrollColumn = 16
 
     createButton                          _
-       range(cells(40,19), cells(41,20)), _
+       range(cells(40,19), cells(41,21)), _
       "Reset Excel Sheet",                _
       "reset_excel_sheet"
+
+    createButton                          _
+       range(cells(43,19), cells(44,21)), _
+      "insert data validation",           _
+      "insert_data_validation"
 
 
     range(cells(38, 18), cells(39,21)).interior.color = rgb(250, 180, 30)
 
+
+end sub ' }
+
+sub insert_data_validation() ' {
+
+    dim rng as range
+    set rng = range(cells(43, 23), cells(44, 26))
+
+    dim firstCellRelativeAddress as string
+    dim formula                  as string
+
+    firstCellRelativeAddress =  rng.address(rowAbsolute := false, columnAbsolute := false)
+    formula                  = "=isNumber(" & firstCellRelativeAddress & ")"
+
+    with rng.validation ' {
+
+        .add type := xlValidateCustom, formula1 := formula
+
+        .ignoreBlank  =  true
+
+        .showInput    =  true
+        .inputTitle   = "Validation rule"
+        .inputMessage = "Enter a numerical value"
+
+        .showError    =  true
+        .errorTitle   = "Validation rule failed"
+        .errorMessage = "Please enter a number""
+
+    end with ' }
+
+    rng.borderAround xlContinuous, xlMedium, color := rgb(140, 90, 180)
+    rng.cells(1,1).offset(-1) = "Validated data (only numbers) in box below"
 
 end sub ' }
